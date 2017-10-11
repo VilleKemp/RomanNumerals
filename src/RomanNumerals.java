d35b28b4bf27728e9bae6315ed8d258582a566a0
VilleKemp <kempo.ville@gmail.com>
2017-10-11 16:33:59 +0300

    Assingment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,148 @@
-
+//Converts roman numbers to integers. Returns 0 if number is not valid.
 public class RomanNumerals {
+		public int invalid;
+		public int value;
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
-		return 0;
+		invalid=0;
+		value=0;
+		if(check_validity(romanNum)!=0) {
+			return 0;
+		}
 		
+		calculate(romanNum);
+		
+		if(invalid!=0) {
+			return 0;
+		}
+		return value;
+		
+	}
+	
+	public int calculate(String s) {
+		
+		
+		for (int i = 0; i < s.length(); i++){
+			char c = s.charAt(i);
+				
+			if(counting_rules(s,i)==0){
+				add(c);
+			}
+			else {
+				substract(c);
+			}
+		}
+		
+		return value;
+	}
+	
+	public int counting_rules(String s, int i) {
+		if(i+1<s.length()) {
+			//I and X can only be substracted from the 2 next highest values
+			if(convert(s.charAt(i))<convert(s.charAt(i+1)) && ( s.charAt(i)=='I') && (convert(s.charAt(i+1))>10) ){
+				invalid=1;
+			}
+			
+			if(convert(s.charAt(i))<convert(s.charAt(i+1)) && ( s.charAt(i)=='X') && convert(s.charAt(i+1))>100 ){
+				invalid=1;
+			}
+			
+			//V,L,D can't appear before number that are larger than they are
+			if(convert(s.charAt(i))<convert(s.charAt(i+1)) && ( s.charAt(i)=='V' || s.charAt(i)=='L' || s.charAt(i)=='D') ){
+				invalid=1;
+			}
+				
+		//smaller then larger
+			
+		if(convert(s.charAt(i))<convert(s.charAt(i+1)) && ( s.charAt(i)!='V' || s.charAt(i)!='L' || s.charAt(i)!='D') ){
+			
+			return 1;
+			
+		}
+
+		}
+		return 0;
+	}
+	
+	
+	public void substract(char c) {
+		value=value-convert(c);
+	}
+	
+	public void add(char c) {
+		
+		value=value+convert(c);
+	}
+	
+
+	
+	public int convert(char c) {
+		int int_value=0;
+		if(c=='I') {
+			int_value=1;
+		}
+		if(c=='V') {
+			int_value=5;
+		}
+		if(c=='X') {
+			int_value=10;
+		}
+		if(c=='L') {
+			int_value=50;
+		}
+		if(c=='C') {
+			int_value=100;
+		}
+		if(c=='D') {
+			int_value=500;
+		}
+		if(c=='M') {
+			int_value=1000;
+		}
+		
+		return int_value;
+	}
+	
+	public int check_validity(String s) {
+
+		for (int i = 0; i < s.length(); i++){
+				char c = s.charAt(i);        
+				repetition(c,s,i);	
+			}
+		if(invalid!=0) {
+		return 1;
+		}
+		else {
+			return 0 ;
+		}
+	}
+	
+	public void repetition(char c, String s, int i){
+		if((i+3)<s.length()) {
+		if(c=='M' && s.charAt(i+1)=='M' && s.charAt(i+2)=='M' && s.charAt(i+3)=='M' ) {
+			invalid = 1;
+		}
+		if(c=='C' && s.charAt(i+1)=='C' && s.charAt(i+2)=='C' && s.charAt(i+3)=='C' ) {
+			invalid = 1;
+		}
+		if(c=='X' && s.charAt(i+1)=='X' && s.charAt(i+2)=='X' && s.charAt(i+3)=='X') {
+			invalid = 1;
+		}
+		if(c=='I' && s.charAt(i+1)=='I' && s.charAt(i+2)=='I' && s.charAt(i+3)=='I') {
+			invalid = 1;
+		}
+		}
+		if((i+1)<s.length()) {
+		if(c=='V' && s.charAt(i+1)=='V' ) {
+			invalid = 1;
+		}
+		if(c=='L' && s.charAt(i+1)=='L' ) {
+			invalid = 1;
+		}
+		if(c=='D' && s.charAt(i+1)=='D' ) {
+			invalid = 1;
+		}
+		
+		}
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,167 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_IXCM_repetition() {
+		RomanNumerals roma = new RomanNumerals();
+		String Ix3 = "IIII" ;
+		String Xx3 = "XXXX" ;
+		String Cx3 = "CCCC" ;
+		String Mx3 = "MMMM" ;
+		int code= roma.convertToInteger(Ix3);
+		assertEquals(0, code);
+		code= roma.convertToInteger(Xx3);
+		assertEquals(0, code);
+		code= roma.convertToInteger(Cx3);
+		assertEquals(0, code);
+		code= roma.convertToInteger(Mx3);
+		assertEquals(0, code);
+		
+		String no_repetition = "XIII";
+		code= roma.convertToInteger(no_repetition);
+		assertEquals(13, code);
+	
 	}
 
+	@Test
+	public void test_VLD_repetition() {
+		RomanNumerals roma = new RomanNumerals();
+		String V = "XVVII" ;
+		String L = "ILLII" ;
+		String D = "DDDDD" ;
+		String valid = "DLV" ;
+		int code= roma.convertToInteger(V);
+		assertEquals(0, code);
+		code= roma.convertToInteger(L);
+		assertEquals(0, code);
+		code= roma.convertToInteger(D);
+		assertEquals(0, code);
+		code= roma.convertToInteger(valid);
+		assertEquals(555, code);
+	
+	}
+	
+	@Test
+	public void test_basic_numbers() {
+		RomanNumerals roma = new RomanNumerals();
+		String I = "I" ;
+		String V = "V" ;
+		String X = "X" ;
+		String L = "L" ;
+		String C = "C" ;
+		String D = "D" ;
+		String M = "M" ;
+		
+		int code= roma.convertToInteger(I);
+		assertEquals(1, code);
+		
+		code= roma.convertToInteger(V);
+		assertEquals(5, code);
+		
+		code= roma.convertToInteger(X);
+		assertEquals(10, code);
+		
+		code= roma.convertToInteger(L);
+		assertEquals(50, code);
+		
+		code= roma.convertToInteger(C);
+		assertEquals(100, code);
+		
+		code= roma.convertToInteger(D);
+		assertEquals(500, code);
+		
+		code= roma.convertToInteger(M);
+		assertEquals(1000, code);
+	
+	}
+	
+	@Test
+	public void test_larger_basic_numbers() {
+		RomanNumerals roma = new RomanNumerals();
+		String s3 = "III" ;
+		String s2012 = "MMXII" ;
+		String s55 = "LV" ;
+
+		
+		int code= roma.convertToInteger(s3);
+		assertEquals(3, code);
+		
+		code= roma.convertToInteger(s2012);
+		assertEquals(2012, code);
+		
+		code= roma.convertToInteger(s55);
+		assertEquals(55, code);
+
+	
+	}
+	
+	@Test
+	public void test_substraction() {
+		RomanNumerals roma = new RomanNumerals();
+		String s4 = "IV" ;
+		String s1984 = "MCMLXXXIV" ;
+
+
+		
+		int code= roma.convertToInteger(s4);
+		assertEquals(4, code);
+		
+		code= roma.convertToInteger(s1984);
+		assertEquals(1984, code);
+
+
+	
+	}
+	
+	@Test
+	public void test_invalid_substraction() {
+		RomanNumerals roma = new RomanNumerals();
+		String s1 = "MVX" ;
+		String s2 = "LC";
+		String s3 = "DM";
+		
+		int code= roma.convertToInteger(s1);
+		assertEquals(0, code);
+		
+		code= roma.convertToInteger(s2);
+		assertEquals(0, code);
+
+		code= roma.convertToInteger(s3);
+		assertEquals(0, code);
+	
+	}
+	
+	
+	@Test
+	public void test_invalid_substraction_IX() {
+		RomanNumerals roma = new RomanNumerals();
+		String s1 = "MIL" ;
+		String s2 = "XD";
+		
+		int code= roma.convertToInteger(s1);
+		assertEquals(0, code);
+		
+		code= roma.convertToInteger(s2);
+		assertEquals(0, code);
+
+
+	
+	}
+	
+	@Test
+	public void test_random_values() {
+		RomanNumerals roma = new RomanNumerals();
+		String s1 = "MMMDXLI" ;
+		String s2 = "CXII";
+		
+		int code= roma.convertToInteger(s1);
+		assertEquals(3541, code);
+		
+		code= roma.convertToInteger(s2);
+		assertEquals(112, code);
+
+
+	
+	}
+		
+	
 }
